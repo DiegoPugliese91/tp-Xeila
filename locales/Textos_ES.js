68b6f8ddb305ced5cfe086526d52ef708f77df22
Diego Pugliese <diego_pugliese91@hotmail.com>
2023-12-30 19:23:38 -0300

    first commit

---
+++ locales/Textos_ES.js
@@ -0,0 +1,75 @@
+let textos_ES={
+
+    // mensajes BACK
+    // ---------------------------------
+    'SQL_OK':'',
+    'SQL_KO':'',
+    //'ATRIBUTO_es_nulo_KO':'', significa que un atributo necesario no se ha enviado
+    'dni_es_nulo_KO':'No se ha enviado el dni',
+    //'ERROR_UPLOAD_ATRIBUTO_KO':'', significa que un atributo de tipo file no cumple condiciones de extension o tamaño, viene descrito en el resource
+    'ERROR_UPLOAD_foto_persona_KO':'La foto de persona debe tener extension jpg o jpeg y tamaño menor de 200000',
+    'controlador_invalido':'',
+    'action_invalido':'',
+
+    // mensajes de la entidad persona
+    // ---------------------------------
+
+    // atributos lirbos (usados en el titulo de la tabla de presentación de datos)
+
+    'CodigoL':'Codigo',
+    'AutoresL':'Autor',
+    'TituloL':'Título',
+    'ISBN':'ISBN',
+    'PagIniL':'Pag inicial',
+    'PagFinL':'Pag Final',
+    'VolumenL':'VolumenL',
+    'EditorialL':'Editorial',
+    'FechaPublicacionL':'FechaPublicacionL',
+    'EditorL':'Editor',
+    'PaisEdicionL':'País edición',
+    'archivopdfL':'archivo PDF',
+
+    // label atributos libro
+    
+    'label_cod_libro':'Introduzca código : ',
+    'label_autor_libro':'Introduzca Nombre : ',
+    'label_titulo_libro':'Introduzca Título : ',
+    'label_ISBN_libro':'Introduzca ISBN : ',
+    'label_PagIni_libro':'Introduzca página inicial : ',
+    'label_PagIfin_libro':'Introduzca página final : ',
+    'label_volumen_libro':'Introduzca volumen : ',
+    'label_editorial_libro':'Introduzca editorial : ',
+    'label_fecha_libro':'Introduzca fecha : ',
+    'label_editor_libro':'Introduzca editor : ',
+    'label_pais_edicion_libro':'edición : ',
+    'label_archivo_pdf':'Introduzca pdf : ',
+
+    // titulo general pagina persona
+    
+    'titulo_pagina_personas':'Gestión de Personas',
+
+    // titulos formularios acciones
+
+    "titulo_form_ADD_persona":"Incorporar un libro",
+    "titulo_form_EDIT_persona":"Modicar persona",
+    "titulo_form_DELETE_persona":"Confirmar borrar una persona",
+    "titulo_form_SEARCH_persona":"Buscar una persona",
+    "titulo_form_SHOWCURRENT_persona":"Ver detalle de una persona",
+   
+    // errores de formato de persona en front
+
+    // nombre_persona
+    "KO_nombre_persona_tam_min":"nombre persona peta minimo",
+    "KO_nombre_persona_tam_max":"nombre persona peta maximo",
+
+    // apellidos_persona
+    "KO_apellidos_persona_tam_min":"apellidos persona peta minimo",
+    "KO_apellidos_persona_tam_max":"apellidos persona peta maximo",
+
+    // direccion_persona
+    "KO_direccion_persona_tam_min":"direccion persona peta minimo",
+    "KO_direccion_persona_tam_max":"direccion persona peta maximo",   
+
+
+};
+
